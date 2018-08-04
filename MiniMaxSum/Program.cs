1628d3666f90cf9f136b290807f7d42d8ac266bb
George Smircea <smircea.george@gmail.com>
2018-08-04 17:50:45 +0300

    first

--- MiniMaxSum/Program.cs
+++ MiniMaxSum/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace MiniMaxSum
+class Solution
 {
-    class Program
+
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(int[] arr)
     {
-        static void Main(string[] args)
+        int n = arr.Length;
+        Int64 min = 900000000000;
+        Int64 max = -90000000000;
+        Int64 s = 0;
+        for (int i = 0; i < n; i++)
         {
+            for (int j = 0; j < n; j++)
+            {
+                s = s + arr[j];
+            }
+            s = s - arr[i];
+            if (s > max) max = s;
+            if (s < min) min = s;
+            s = 0;
         }
+        Console.WriteLine(min + " " + max);
+
+
+    }
+
+    static void Main(string[] args)
+    {
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        miniMaxSum(arr);
     }
 }
